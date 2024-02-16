1685732fdb71098f3a0f3396e7ec1b65ae128088
edisonfernandez <edison.fernandez@pollinate.net>
2024-02-16 18:10:30 -0500

    feat(structure): add project folder structure clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActorsComponent } from './actors/actors.component';
+import { CreateupdatedActorComponent } from './ui/view-models/createupdated-actor/createupdated-actor.component';
+import { CreateUpdateComponent } from './ui/view-models/actor/create-update/create-update.component';
+import { ListComponent } from './ui/view-models/actor/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorsComponent,
+    CreateupdatedActorComponent,
+    CreateUpdateComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
